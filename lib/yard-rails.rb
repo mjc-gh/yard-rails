74236e5f68b9ef1a369316b09ab9d374b9de1649
Charles Pence <charles@charlespence.net>
2012-08-01 10:52:53 -0400

    Silly bug wasn't getting attr_accessible loaded as a gem.
    This closes #1.

--- lib/yard-rails.rb
+++ lib/yard-rails.rb
@@ -1,2 +1,5 @@
 require 'yard-rails/class_attribute_handler'
 require 'yard-rails/legacy/class_attribute_handler'
+
+require 'yard-rails/attr_accessible_handler'
+require 'yard-rails/legacy/attr_accessible_handler'
